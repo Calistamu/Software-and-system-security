59345375ffaec37d361605792ddee19554e7053c
mudou <41284875+Calistamu@users.noreply.github.com>
2020-03-31 22:46:46 +0800

    插入调好了

--- test0x02-webwriting/code/httpserver.py
+++ test0x02-webwriting/code/httpserver.py
@@ -34,6 +34,7 @@ class MyHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         grades='OK'
+        flag=0
         form_data = cgi.FieldStorage(
             fp=self.rfile,
             headers=self.headers,
@@ -42,24 +43,28 @@ class MyHTTPRequestHandler(BaseHTTPRequestHandler):
                 'CONTENT_TYPE': self.headers['Content-Type'],
             })
         fields = form_data.keys()
+        
         if self.field_name in fields:
             input_data = form_data[self.field_name].value
             file = open("."+self.path, "wb")
             file.write(input_data.encode())
+            flag=0
         
-        elif 'ins' in fields: # 当前为录入成绩
-            course_id, student_id,grades = form_data['course_d'].value, form_data['student_id'].value, form_data['grades'].value
+        elif 'grades' in fields: 
+            course_id, student_id, grades = form_data['course_id'].value, form_data['student_id'].value, form_data['grades'].value
             conn = sqlite3.connect('edu.db')
             c = conn.cursor()
             sql = "insert into studentsinfo values (%s, %s, %s)"%(course_id, student_id, grades)
             c.execute(sql)
             conn.commit()
             conn.close()
-        else:   # 当前为成绩查询
+            flag=1
+
+        else:   
             course_id, student_id = form_data['course_id'].value, form_data['student_id'].value
             conn = sqlite3.connect('edu.db')
             c = conn.cursor()
-            sql = "select res from studentsinfo where course_id=%s and student_id=%s"%(course_id, student_id)
+            sql = "select grades from studentsinfo where course_id=%s and student_id=%s"%(course_id, student_id)
             c.execute(sql)
             grades = "%s 同学 %s 课程的成绩为 " % (student_id, course_id) + str(c.fetchone()[0])
             conn.close()
@@ -68,7 +73,12 @@ class MyHTTPRequestHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes(str("<html lang='zh-CN'><head><meta charset='utf-8'></head><body>%s</body></html>"%(grades)), 'utf-8'))
+        if flag==0:
+            self.wfile.write(b"<html><body>OK</body></html>")
+        elif flag==1:
+            self.wfile.write(b"<html><head><meta charset='utf-8'></head><body>Insert successfully!</body></html>")
+        else:   
+            self.wfile.write(b"<html><head><meta charset='utf-8'></head><body>%s</body></html>"%(grades))
 
 class MyHTTPServer(HTTPServer):
     def __init__(self, host, port):
